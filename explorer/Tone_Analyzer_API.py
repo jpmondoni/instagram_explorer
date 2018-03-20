1a58504d88e719c9f298146a1657db75e10e348b
Joao P. A. Mondoni <jp.mondoni@gmail.com>
2018-03-20 16:50:43 -0300

    Add sentence tone support.
    Needs to be modified to return comprehensible data to an app, or just return the json nodes to be read later.

--- explorer/Tone_Analyzer_API.py
+++ explorer/Tone_Analyzer_API.py
@@ -5,6 +5,7 @@
  
 import requests
 import json
+from nltk.tokenize import sent_tokenize
 from secret.Watson import auth
  
 def analyze_tone(text):
@@ -14,16 +15,25 @@ def analyze_tone(text):
 	watsonUrl = user['url'] 
 	headers = {"content-type": "text/plain"}
 	data = text
+
 	try:
 		r = requests.post(watsonUrl, auth=(username,password),headers = headers,
 		 data=data)
-		print(r.text)
 		return r.text
 	except:
 		return False
  
-def display_results(data):
+def display_results(text):
+	data = analyze_tone(text)
 	# Display results for document tone. 
 	data = json.loads(str(data))
 	for i in data['document_tone']['tones']:
 		print(i['tone_name'].ljust(20),(str(round(i['score'] * 100,1)) + "%").rjust(10))
+
+	sents = sent_tokenize(text)
+	if(len(sents) > 1):
+		for i in data['sentences_tone']:
+			print(i['sentence_id'])
+			print("Sentence #",i['sentence_id'],i['text'][:40])
+			for j in i['tones']:
+				print(j['tone_name'].ljust(20),(str(round(j['score'] * 100,1)) + "%").rjust(10))
